52a55e953b59f94c4546dbed70aef1990e1fd1d5
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-31 15:19:05 +0100

    inizio creazione condizioni

---
+++ main.js
@@ -0,0 +1,29 @@
+// inizializzo variabili
+const gridElement = document.getElementById('grid');
+const start = document.getElementById('start');
+const difficolta = document.getElementById('selector-difficolta').value;
+
+// al click sul pulsante play mostro a video la tabella a seconda della difficoltà inserita
+ start.addEventListener('click', function(){
+
+    if(difficolta == 0){
+
+        for(let i=1; i<=81; i++){
+
+            const node = document.createElement('div');
+            node.classList.add('square-easy');
+            node.innerHTML += i;
+            node.addEventListener('click', function(){
+        
+                console.log(this);
+                this.classList.add('clicked');
+            })
+        
+            gridElement.appendChild(node);
+        }
+
+    }
+
+
+ })
+
